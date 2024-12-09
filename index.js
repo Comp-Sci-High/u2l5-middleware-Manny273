9a057b049a018c603dffc6af1a8a6337675e47c9
Manny273 <134006013+Manny273@users.noreply.github.com>
2024-12-09 20:24:33 +0000

    Finished

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const app = express();
 
 // Step 2. Before the first route, create middleware that console logs the request method and request url
 // Make sure to call next() inside the callback function
-
+app.use((request,response,next) => {
+  console.log(request.method + "/" + request.url )
+  next()
+})
 // Step 3. Run your server and test the routes /, /pasta, & /pizza
 // What do you see being console logged?
 
-// Step 4. Create middleware that console logs "YUM" only for the routes /pasta & /pizza
+// Step 4.{} Create middleware that console logs "YUM" only for the routes /pasta & /pizza
 
 // Step 5. Create middleware that catches all undefined routes, and sends back "404 NOT FOUND" in an h1 tag
 // Does the placement of this middleware mattter?
@@ -17,7 +20,10 @@ const app = express();
 app.get("/", (req, res) => {
   res.send(`<h1>Welcome to my resturant</h1>`);
 });
-
+app.use((request,response,next) => {
+  console.log("YUM")
+  next()
+})
 app.get("/pasta", (req, res) => {
   res.send(`<h1>Pasta</h1><img src="http://bit.ly/3ZUcEpH" width="500px">`);
 });
@@ -25,6 +31,11 @@ app.get("/pasta", (req, res) => {
 app.get("/pizza", (req, res) => {
   res.send(`<h1>Pizza</h1><img src="https://bit.ly/3ZH9OUL" width="500px">`);
 });
+
+app.use((request,response,next) => {
+  response.send(`404 Not found!`)
+})
+
 
 app.listen(3000, () => {
   console.log(`Server running at http://localhost:3000`);
